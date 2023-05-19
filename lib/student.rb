9f3857d8c92cd629e17edd59ad498f1974ace388
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-20 02:14:00 +0300

    Completed assingments

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+    def initialize
+        @knowledge = []
+    end
+    def learn value
+        @knowledge << value
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
